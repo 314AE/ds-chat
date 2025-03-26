022584d5cba2e2d1ca83bf707f3d0b2646b84c5c
xiaoWu <3424076473@qq.com>
2025-03-26 14:31:42 +0800

    隐藏api key

--- ds_chat_project/ai_make_chat/_make_ai_chat_.py
+++ ds_chat_project/ai_make_chat/_make_ai_chat_.py
@@ -5,7 +5,8 @@ class MakeAIChat(object):
     def createAIChat(self):
         if self.__chatContent is not None:
             # for backward compatibility, you can still use `https://api.deepseek.com/v1` as `base_url`.
-            client = OpenAI(api_key="sk-71e390e1b3aa43ecad218db9670a3da4", base_url="https://api.deepseek.com")
+            # 填写自己的deepseek创建的api_key
+            client = OpenAI(api_key="XXXXXXXXXXX", base_url="https://api.deepseek.com")
 
             response = client.chat.completions.create(
                 model="deepseek-chat",
